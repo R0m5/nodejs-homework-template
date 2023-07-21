cce9e63cd09bf4e8f26210b191fa1decbfc59b07
R0m5 <rlastovetskyi@gmail.com>
2023-07-21 17:22:30 +0300

    +

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+// pvWUvJ5yB3kVFyOR;
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+// const mongoose = require('mongoose');
+require("dotenv").config();
 
-const contactsRouter = require('./routes/api/contacts')
+// const DB_HOST = "mongodb+srv://Iryna:CLCOvKYzTKXeVYLN@cluster0.4aldtzj.mongodb.net/03-mongodb?retryWrites=true&w=majority";
+// mongoose.set('strictQuery', true);
 
-const app = express()
+// mongoose.connect(DB_HOST)
+// .then(() => console.log("Database connect success"))
+// .catch(error => console.log(error.message))
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const contactsRouter = require("./routes/api/contacts");
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use('/api/contacts', contactsRouter)
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
